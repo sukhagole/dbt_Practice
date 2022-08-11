50f4a32e7fa16ba471cd88bb1483da477da0bf86
Sukharam Gole <sukharam.gole@infosys.com>
2022-08-11 18:35:49 +0000

    Update the code for snapshot where Jinja is being used to create a new, dedicated schema

---
+++ snapshots/mock_orders.sql
@@ -0,0 +1,20 @@
+{ % snapshot mock_orders %} 
+	
+    {% set new_schema = target.schema _ '_snapshot' %}
+
+	{{ 
+		config(
+			target_database ='analytics',				
+			target_schema = 'new_schema',								// These target databse and schema are different from our default target values run for dbt projects. We want this to stay somewhere independently 
+			unique_key ='order_id',
+			
+			strategy ='timestamp',												// we are using the timstamp to determine whether the time record has been add or changed. 
+			updated_at ='updated_at',
+			)
+			
+	}}
+	
+	select * from analytics.{{ target.schema}}.mock_orders
+	
+	{% endsnapshot %}
+	
